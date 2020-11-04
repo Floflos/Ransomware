90093aeda87972686d850f18390efefe47e00bd2
camille77 <camille77680@gmail.com>
2020-11-03 19:02:01 -0500

    Ajout de commentaires

--- serveur.py
+++ serveur.py
@@ -6,13 +6,13 @@ import http.server
 import secrets
  
 def main(argv,argc):
-	cle = secrets.token_hex(32)
+	cle = secrets.token_hex(32) #clé générée aléatoirement 
 	print (cle)
-	fichier = open("cle.txt", "w")
-	fichier.write("key :"+cle)
+	fichier = open("cle.txt", "w") #création et ouverture du fichier cle.txt
+	fichier.write("key :"+cle) #on écrit la clé 
 	fichier.close()
 
-	PORT = 8080
+	PORT = 6666 #utilisation du port 6666
 	address = ("", PORT)
 
 	server = http.server.HTTPServer
@@ -21,7 +21,7 @@ def main(argv,argc):
 
 	handler.cgi_directories = ["./"]
 
-	httpd = server(address, handler)
+	httpd = server(address, handler) #lancement du serveur
 	print("Serveur actif sur le port :", PORT)
 	print("Pour les conditions du tp, laissez le serveur ouvert pendant toute l'operation (chiffrement et dechiffrement)")
 	print("En effet, une clé aleatoire est generée à chaque fois")
